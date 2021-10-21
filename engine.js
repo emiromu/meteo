07eadb05d67f64b99b04ee2f4a2279fa87fb7163
Emilien Romulus <emilien.romulus@tuta.io>
2021-10-21 18:07:47 +1100

    City name handling and display of promise resolve and reject

--- engine.js
+++ engine.js
@@ -22,13 +22,78 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=bPLCz3lzCRECdim17wMqZDbbH
     getCats();
 */
 
+/**Free public API Key, safe to let public for the scope of the project*/
 const key = '484e301936b57b282774bca33781f11b';
 
-function getCityData(city,key){
+function createWeatherCard(apiData){
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    /**Wind speed scale */
+    /*
+    Metric: 
+    Light winds	19 km/h or less
+4	Moderate winds	20 - 29 km/h
+5	Fresh winds	30 - 39 km/h
+6	Strong winds	40 - 50 km/h
+
+    Imperial: 
+    11 mph or less
+    11+ to 18 mph
+    18+ to 24 mph
+    24+ to 31 mph
+    */
+    const weatherCard = {
+        name:apiData.name,
+        temperature:apiData.main.temp,
+        temperature_max:apiData.main.temp_max,
+        temperature_min:apiData.main.temp_min,
+        humidty:apiData.main.humidity,
+        windspeed:apiData.wind.speed,
+        description:apiData.weather[0].description
+    };
+
+
+    return weatherCard
 };
 
-getCityData('Paris',key);
+//units : standard, metric, imperial
+async function getCityData(cityName,key,units){
+    if(units == undefined)units='standard';
+    let weatherCard = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=${units}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return createWeatherCard(data)
+    })
+    return weatherCard;
+};
+
+
+//let testData = getCityData('Paris',key,'metric')
+//console.log(testData);
+
+const cityInput = document.querySelector(`#cityInput`);
+const searchButton = document.querySelector(`#searchButton`);
+const weatherPannel = document.querySelector(`#weatherPannel`);
+
+searchButton.addEventListener(`click`,function(e){
+    console.log('Click Event');
+    getCityData(cityInput.value,key,'metric')
+    .then(data=>{
+        console.log(data)
+        weatherPannel.innerHTML=JSON.stringify(data);
+    })
+    .catch(error=>{
+        //'400' 'Nothing to geocode'
+        //'404' 'city not found'
+        console.log(error.message)
+        weatherPannel.innerHTML=error.message;
+    });
+});
+
+cityInput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        console.log('Enter keyup event');
+        searchButton.click();
+    }
+});
